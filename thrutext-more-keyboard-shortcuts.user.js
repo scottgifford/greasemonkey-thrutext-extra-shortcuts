16ace87334ea33e9b8f6cc94c430a89538528b34
Scott Gifford <sgifford@suspectclass.com>
2020-10-04 18:27:19 -0400

    Use Meta instead of CTRL on this branch.

--- thrutext-more-keyboard-shortcuts.user.js
+++ thrutext-more-keyboard-shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ThruText More Keyboard Shortcuts
 // @namespace    http://suspectclass.com/
-// @version      0.8
+// @version      0.8meta
 // @resource helpHtml https://github.com/scottgifford/greasemonkey-thrutext-extra-shortcuts/raw/master/help.html
 // @description  Add extra keyboard shortcuts for ThruText
 // @author       Scott Gifford <sgifford@suspectclass.com>
